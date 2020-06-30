d3f7d8c05fe1eb2f7d57f14b9aeada470ca12959
Dychkevych Kolya <dychkevych.kolya@gmail.com>
2020-06-30 19:36:09 +0300

    main page

--- app/js/index.js
+++ app/js/index.js
@@ -1,7 +1,15 @@
 import 'normalize.css';
-
+import './select-list';
 
 $(document).ready((e) => {
+
+    $('#main-slider').owlCarousel({
+      loop:true,
+      margin:10,
+      nav:true,
+      dots:false,
+      items: 1
+  });
 
 });
 
