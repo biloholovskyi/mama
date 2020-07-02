81a8c02e189ceda8d8ef0cbc2ac6cc6b42ded21b
Dychkevych Kolya <dychkevych.kolya@gmail.com>
2020-07-02 22:03:43 +0300

    main page

--- app/js/index.js
+++ app/js/index.js
@@ -4,14 +4,84 @@ import './select-list';
 $(document).ready((e) => {
 
     $('#main-slider').owlCarousel({
+      
       loop:true,
       margin:10,
       nav:true,
       dots:false,
-      items: 1
+      items: 1,
+      animateOut: 'fadeOut',
+      animateIn: 'fadeIn'
+     
+      // smartSpeed:450
+      
+  });
+
+  $('.menu').on('click', function(){
+    $(this).children('.romb').toggleClass('active'); 
+    $('.modal-menu').toggleClass('active');
+    $('body').toggleClass('active');
+  
+  });
+
+
+  // button on modal item
+  $('.count-dish').on('click', function(){
+    $(this).toggleClass('active');
+  });
+
+  // click on card food
+  $('.dish-item').on('click', function(){
+      const current = $(this);
+
+      const img    = current.children('.food-img').attr('src');
+      const title  = current.children('.dish-title').find('h4').html();
+      const title2 = current.children('.dish-title').find('.bold').html();
+      const price  = current.children('.dish-list-wrap').find('.busket').html();
+
+      $('.modal-img').css('background-image', 'url(' + img + ')');
+      $('.modal-info h4').html(title);
+      $('.modal-info h3').html(title2);
+      $('.info-dish .price').html(price);
+
+      $('.dish-overlay').css('visibility', 'visible');
+      $('body').css({
+        'overflow': 'hidden',
+        'position': 'relative',
+        'height': '100vh'
+      });
+  });
+
+  $('.modal-close').on('click', function () {
+    $('.dish-overlay').css('visibility', 'hidden');
+    $('body').css({
+      'overflow': 'visible',
+      'position': 'relative',
+      'height': '100%'
+    });
   });
 
 });
+
+
+$(document).mouseup(function(e){
+  let div = $('.modal-menu, .menu , .romb');
+  if(!div.is(e.target)
+  && div.has(e.target).length === 0) {
+    div.removeClass('active');
+  }
+});
+
+// $(document).on('click', function(){
+//   let modal = $('.modal-content');
+//   let btn = $('.dish-item');
+
+//   if(!btn.is(e.target) && btn.has(e.target).length === 0) {
+//     if(!modal.is(e.target) && modal.has(e.target).length === 0) {
+//       $('.dish-overlay').css('visibility', 'hidden');
+//       }
+//     }   
+// });
 
 $(window).resize(() => {
 
