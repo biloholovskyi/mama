8de1f4331cf395e528168433090c6b001af66251
Biloholovskyi <biloholovskyi>
2020-07-27 16:48:32 +0300

    cart

--- app/wp-content/themes/mama/js/cart.js
+++ app/wp-content/themes/mama/js/cart.js
@@ -4,36 +4,45 @@ class Cart {
     this.busketBody = document.querySelector('.itemInBusket-body');
   }
 
-  creatOrder (e) {
+  creatOrder(e) {
+    this.smallCart.style.display = 'none';
     $('.dish-overlay').css('visibility', 'hidden');
 
     $('.addbusket-modal').css('visibility', 'visible');
 
-    setTimeout(function() {
+    setTimeout(() => {
       $('.addbusket-modal').css('visibility', 'hidden');
-      $('.inbusket').css('display', 'flex');
+      this.smallCart.style.display = 'flex';
+      this.renderOrder();
     }, 4000);
 
     const date = new Date();
     const lifeTime = date.getTime() + (24 * 60 * 60 * 1000);
 
+    const storeOrder = localStorage.getItem('mama-order');
+
     let listOrder = []
+
+    if (storeOrder) {
+      const storeOrderParse = JSON.parse(storeOrder);
+      listOrder = storeOrderParse.listOrder;
+    }
 
     const newItemOrder = {
       id: e.target.getAttribute('data-product'),
-      count: e.target.getAttribute('data-count')
+      count: +e.target.getAttribute('data-count') > 0 ? e.target.getAttribute('data-count') : 1
     }
 
     listOrder.push(newItemOrder);
 
     const order = {
-     lifeTime,
+      lifeTime,
       listOrder
     }
 
     const orderJSON = JSON.stringify(order);
 
-    if(localStorage.getItem('mama-order') !== null) {
+    if (localStorage.getItem('mama-order') !== null) {
       localStorage.removeItem('mama-order');
       localStorage.setItem('mama-order', orderJSON);
     } else {
@@ -42,14 +51,17 @@ class Cart {
     }
   }
 
-  renderOrder () {
-    if(localStorage.getItem('mama-order') !== null) {
+  renderOrder() {
+    this.busketBody.querySelectorAll('.item').forEach(item => {
+      item.remove()
+    });
+
+    if (localStorage.getItem('mama-order') !== null) {
       const orderJSON = localStorage.getItem('mama-order');
       const order = JSON.parse(orderJSON);
       const date = new Date();
       const currentTime = date.getTime();
-
-      if(currentTime > order.lifeTime) {
+      if (currentTime > order.lifeTime || order.listOrder.length < 1) {
         localStorage.removeItem('mama-order');
       } else {
         this.smallCart.style.display = 'flex';
@@ -63,7 +75,7 @@ class Cart {
           const gram = cart.getAttribute('data-gram');
           const img = cart.querySelector('.food-img').getAttribute('src');
           const price = parseInt(cart.querySelector('.busket').innerHTML);
-          allPrice += price;
+          allPrice += price * count;
 
           const newItem = document.createElement('div');
           newItem.classList.add('item');
@@ -115,6 +127,7 @@ class Cart {
 
           const dlt = document.createElement('div');
           dlt.className = 'dlt-item';
+          dlt.setAttribute('data-id', id);
 
           right.append(dlt);
           newItem.append(left);
@@ -125,135 +138,55 @@ class Cart {
       }
     }
   }
+
+  removeOrder(e) {
+    const id = e.target.getAttribute('data-id');
+    let store = localStorage.getItem('mama-order');
+    store = JSON.parse(store);
+    const index = store.listOrder.findIndex(item => item.id.toString() === id.toString());
+    const newList = [...store.listOrder.slice(0, index), ...store.listOrder.slice(index + 1)];
+
+    const date = new Date();
+    const lifeTime = date.getTime() + (24 * 60 * 60 * 1000);
+
+    let newStore = {
+      lifeTime,
+      listOrder: newList
+    }
+
+    newStore = JSON.stringify(newStore);
+    localStorage.removeItem('mama-order');
+    localStorage.setItem('mama-order', newStore);
+    e.target.parentElement.parentElement.remove();
+    document.querySelector('.item-in-busket').style.display = 'flex';
+    this.smallCart.style.display = 'none';
+  }
+
+  onCount(e) {
+    const type = e.target.classList.contains('plus');
+
+    const id = e.target.parentElement.nextElementSibling.getAttribute('data-id');
+
+    let store = localStorage.getItem('mama-order');
+    store = JSON.parse(store);
+    const index = store.listOrder.findIndex(item => item.id.toString() === id.toString());
+    let count = type ? +store.listOrder[index].count + 1 : +store.listOrder[index].count - 1;
+    if(count < 1) {
+      count = 1;
+    }
+    const newOrder = {
+      ...store.listOrder[index],
+      count
+    }
+    const newList = [...store.listOrder.slice(0, index), newOrder, ...store.listOrder.slice(index + 1)];
+    const newStore = {
+      lifeTime: store.lifeTime,
+      listOrder: newList
+    }
+    localStorage.removeItem('mama-order');
+    localStorage.setItem('mama-order', JSON.stringify(newStore));
+    this.renderOrder();
+  }
 }
 
 export default Cart;
-
-const wrapper = $('.instagram-post__wrapper');
-
-// вывод данных в DOM
-const renderItems = () => {
-  // получаем массив из стора
-  const items = JSON.parse(localStorage.getItem('insta-array'));
-
-  // сортируем его по времени
-  items.sort((a, b) => a.index - b.index);
-
-  let counter = 0;
-
-  // перебираем его и выводим на странице
-  items.forEach(item => {
-    counter++;
-    if (counter < 10) {
-      wrapper.append('<a href="' + item.permalink + '" class="item-instagram item-instagram--visible" target="_blank"><img src="' + item.url + '" alt="forestpark"></a>')
-    } else {
-      wrapper.append('<a href="' + item.permalink + '" class="item-instagram" target="_blank"><img src="' + item.url + '" alt="forestpark"></a>')
-    }
-  });
-};
-
-// API
-const getInstPost = () => {
-  console.log('api connection');
-  // получаем список id последних постов
-  $.ajax({
-    url: 'https://graph.instagram.com/17841405715433625?fields=id,username,media&access_token=IGQVJXVTVfNzg1RkhQREh0MXh0UzFLMW9zZAXNIVS1Ha1pWcEZAvVWFfSHZAjMjBNaHVkaFJWcEJjcXdnQTJ5dUt6ejBaQ0VlRHVWSVk2NGRzRWkzOGI4ZAHJIbWJ0NE9vT1cxNVNBdEJ3',
-    dataType: 'jsonp',
-    type: 'GET',
-    success: function(data){
-      // массив id постов
-      const idArray = data.media.data;
-      const idArrayCount = idArray.length;
-      let counter = 0;
-
-      // массив постов
-      const mediaStore = [];
-
-      // получаем посты каждого id
-      idArray.forEach((element, index) => {
-        $.ajax({
-          url: 'https://graph.instagram.com/' + element.id + '?fields=id,media_type,media_url,username,timestamp,permalink&access_token=IGQVJXVTVfNzg1RkhQREh0MXh0UzFLMW9zZAXNIVS1Ha1pWcEZAvVWFfSHZAjMjBNaHVkaFJWcEJjcXdnQTJ5dUt6ejBaQ0VlRHVWSVk2NGRzRWkzOGI4ZAHJIbWJ0NE9vT1cxNVNBdEJ3',
-          dataType: 'jsonp',
-          type: 'GET',
-          success: function(data){
-            // проверяем что пост не содержит видео
-            counter++;
-            if(data.media_type !== 'VIDEO') {
-              // добавляем медиа в массив
-              mediaStore.push({'index': index, 'permalink': data.permalink, 'url': data.media_url, 'time': data.timestamp});
-
-              // Добавляем в стор кэшированный массив
-              // создаем json
-              const jsonMedia = JSON.stringify(mediaStore);
-
-              // если стор существует удаляем его
-              if(localStorage.getItem('insta-array') !== null) {
-                localStorage.removeItem('insta-array');
-
-                // добавляем в стор новый массив
-                localStorage.setItem('insta-array', jsonMedia);
-              } else {
-                // добавляем в стор новый массив
-                localStorage.setItem('insta-array', jsonMedia);
-              }
-
-              if(counter === idArrayCount) {
-                // вызываем метод рендера
-                setTimeout(() => {
-                  renderItems();
-                }, 5000);
-              }
-            }
-          }
-        });
-      });
-    }
-  });
-};
-
-// подключение к локал стор и проверка последнего кэширования
-const checkStore = () => {
-  // проверяем та ли страница
-  if(wrapper.length > 0) {
-    const instaStatus = localStorage.getItem('insta-status');
-    console.log(`Status = ${instaStatus}`);
-    const today = new Date();
-    const newStatus = today.getDate();
-
-    // если нету создаем новый
-    if(instaStatus === null) {
-      // добавляем статус в стор
-      localStorage.setItem('insta-status', newStatus.toString());
-
-      // обращаемся к API
-      getInstPost();
-    } else {
-      // если есть проверяем совпадает ли он с текущим
-      if(instaStatus.toString() === newStatus.toString()) {
-        // проверяем есть ли стор с массивом данных
-        if(localStorage.getItem('insta-array') !== null) {
-          // если есть вызываем рендер
-          renderItems();
-        } else {
-          // если нет обращаемся к API
-          getInstPost();
-        }
-      } else {
-        // Удаляем и заменяем старый статус в сторе
-        localStorage.removeItem('insta-status');
-        localStorage.setItem('insta-status', newStatus.toString());
-
-        // обращаемся к API
-        getInstPost();
-      }
-    }
-  }
-};
-
-// подгрузка больше постов
-const morePost = () => {
-  $('.item-instagram').addClass('item-instagram--visible');
-  $(event.currentTarget).remove();
-};
-
-export {checkStore, morePost}
--- app/wp-content/themes/mama/js/index.js
+++ app/wp-content/themes/mama/js/index.js
@@ -8,33 +8,36 @@ $(document).ready((e) => {
   cart.renderOrder();
 
   $('#tabs .tab').on('click', (e) => switchTabs(e));
+  $('body').on('click', '.item-in-busket .itemInBusket-body .item .dlt-item',  (e) => cart.removeOrder(e));
+  $('body').on('click', '.item-in-busket .itemInBusket-body .item .count-wrap .plus', (e) => cart.onCount(e));
+  $('body').on('click', '.item-in-busket .itemInBusket-body .item .count-wrap .minus', (e) => cart.onCount(e));
 
 
   //anhors 
-  $('.anhors').on('click', function(event){
+  $('.anhors').on('click', function (event) {
     event.preventDefault();
-    let id  = $(this).attr('href'),
-        top = $(id).offset().top;
+    let id = $(this).attr('href'),
+      top = $(id).offset().top;
 
-     $('body, html').animate({scrollTop: top}, 800);
-     $('.header').removeClass('active');
+    $('body, html').animate({scrollTop: top}, 800);
+    $('.header').removeClass('active');
   });
 
   $('#main-slider').owlCarousel({
-      loop:true,
-      margin:10,
-      nav:true,
-      dots:false,
-      mouseDrag: false,
-      touchDrag: false,
-      items: 1,
-      animateOut: 'fadeOut',
-      animateIn: 'fadeIn'
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    mouseDrag: false,
+    touchDrag: false,
+    items: 1,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn'
   });
 
 
   // open menu
-  $('.menu').on('click', function(){
+  $('.menu').on('click', function () {
     $(this).children('.romb').toggleClass('active');
     $('.modal-menu').toggleClass('active');
     $('body').toggleClass('active');
@@ -42,53 +45,51 @@ $(document).ready((e) => {
   });
 
 
-
   // close menu on responsive
-  $('.modal-close2').on('click', function(){
+  $('.modal-close2').on('click', function () {
     $('.modal-menu').toggleClass('active');
     $('body').removeClass('active');
   });
 
 
   // select active button on modal item
-  $('.count-dish').on('click', function(){
+  $('.count-dish').on('click', function () {
     const count = parseInt($(this).html().replace(/[^\d]/g, ''))
     $('.addBusket').attr('data-count', count);
-    if(!$(this).hasClass('active')){
+    if (!$(this).hasClass('active')) {
       $(this).siblings().removeClass('active');
       $(this).addClass('active');
     }
   });
 
 
-
   // card food modal
-  $('.dish-item').on('click', function() {
-      const current = $(this);
-      const id = current.attr('id');
+  $('.dish-item').on('click', function () {
+    const current = $(this);
+    const id = current.attr('id');
 
-      const img    = current.children('.food-img').attr('src');
-      const title  = current.children('.dish-title').find('h4').html();
-      const title2 = current.children('.dish-title').find('.bold').html();
-      const gramm = current.children('.dish-title').find('.gramms').html();
-      const price  = current.children('.dish-list-wrap').find('.busket').html();
+    const img = current.children('.food-img').attr('src');
+    const title = current.children('.dish-title').find('h4').html();
+    const title2 = current.children('.dish-title').find('.bold').html();
+    const gramm = current.children('.dish-title').find('.gramms').html();
+    const price = current.children('.dish-list-wrap').find('.busket').html();
 
-      $('.modal-img').css('background-image', 'url(' + img + ')');
-      $('.modal-info h4').html(title);
-      $('.modal-info h3').html(title2);
-      $('.info-dish .price').html(price);
-      $('.info-dish .weight').html(gramm +'g');
-
-
-      $('.small-img').css('background-image', 'url(' + img + ')');
-      $('.addbusket-modal .small-title').find('.kind').html(title);
-      $('.addbusket-modal .small-title').find('.name').html(title2);
+    $('.modal-img').css('background-image', 'url(' + img + ')');
+    $('.modal-info h4').html(title);
+    $('.modal-info h3').html(title2);
+    $('.info-dish .price').html(price);
+    $('.info-dish .weight').html(gramm + 'g');
 
 
-      $('.addBusket').attr('data-product', id);
+    $('.small-img').css('background-image', 'url(' + img + ')');
+    $('.addbusket-modal .small-title').find('.kind').html(title);
+    $('.addbusket-modal .small-title').find('.name').html(title2);
 
-      $('.dish-overlay').css('visibility', 'visible');
-      $('body').addClass('active');
+
+    $('.addBusket').attr('data-product', id);
+
+    $('.dish-overlay').css('visibility', 'visible');
+    $('body').addClass('active');
   });
 
   $('.modal-close').on('click', function () {
@@ -97,23 +98,23 @@ $(document).ready((e) => {
     $('body').removeClass('active');
   });
 
- // close modal busket
- $('.modal-close3').on('click', function () {
-  $('.item-in-busket').css('display', 'none');
-});
+  // close modal busket
+  $('.modal-close3').on('click', function () {
+    $('.item-in-busket').css('display', 'none');
+    cart.renderOrder();
+  });
 
- // modal add on busket
+  // modal add on busket
   $('.addBusket').on('click', (e) => cart.creatOrder(e));
 
 
-
-  $('.inbusket').on('click', function() {
-      $(this).css('display', 'none');
-      $('.item-in-busket').css('display', 'flex');
+  $('.inbusket').on('click', function () {
+    $(this).css('display', 'none');
+    $('.item-in-busket').css('display', 'flex');
   });
 
 
-  $('.contine').on('click', function(){
+  $('.contine').on('click', function () {
     $('.item-in-busket').css('display', 'none');
     $('.order-overlay').css('visibility', 'visible');
     $('body').addClass('active');
@@ -121,50 +122,49 @@ $(document).ready((e) => {
 
 // adaptive menu  open
 
-$('.burger').on('click', function(){
-  $('.header').toggleClass('active');
-  $('body').toggleClass('active');
-});
+  $('.burger').on('click', function () {
+    $('.header').toggleClass('active');
+    $('body').toggleClass('active');
+  });
 
 
 //
 
-$('.salat_menu').on('click', function(){
-  $('.popular-dishes .tabs .salat_menu').addClass('active').siblings().removeClass('active');
+  $('.salat_menu').on('click', function () {
+    $('.popular-dishes .tabs .salat_menu').addClass('active').siblings().removeClass('active');
+  });
+
+
 });
-
-
-
-});
-
-
-
 
 
 // close modal
 
-$(document).mouseup(function(e){
+$(document).mouseup(function (e) {
   let div = $('.modal-menu, .menu , .romb');
-  if(!div.is(e.target)
-  && div.has(e.target).length === 0) {
+  if (!div.is(e.target)
+    && div.has(e.target).length === 0) {
     div.removeClass('active');
     $('body').removeClass('active');
   }
 });
 
-$(document).on('click', function(e){
+$(document).on('click', function (e) {
   let modal = $('.modal-content, .item-in-busket');
-  let btn = $('.dish-item, .contine, .inbusket');
+  let btn = $('.dish-item, .contine, .inbusket, .dlt-item');
+  if(e.target.classList.contains('dlt-item') || e.target.classList.contains('plus') || e.target.classList.contains('minus')) {
+    return
+  }
 
 
-  if(!btn.is(e.target) && btn.has(e.target).length === 0) {
-    if(!modal.is(e.target) && modal.has(e.target).length === 0) {
+  if (!btn.is(e.target) && btn.has(e.target).length === 0) {
+    if (!modal.is(e.target) && modal.has(e.target).length === 0) {
       $('.dish-overlay').css('visibility', 'hidden');
       $('.order-overlay').css('visibility', 'hidden');
       $('.item-in-busket').css('display', 'none');
-      }
+      cart.renderOrder();
     }
-
+  }
 });
 
 $(window).resize(() => {
