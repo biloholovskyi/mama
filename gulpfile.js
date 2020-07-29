a4bc3e941894d2954879f4be9672b5a70dd8d7e7
Biloholovskyi <biloholovskyi>
2020-07-29 11:36:11 +0300

    check

--- gulpfile.js
+++ gulpfile.js
@@ -34,39 +34,39 @@ gulp.task('pug', function () {
     .pipe(gulp.dest('app/build'));
 })
 
-gulp.task('default', function() {
+gulp.task('default', function () {
   browserSync.init({
-        server: {
-            baseDir: "./app/wp-content/themes/mama"
-        } 
-    // proxy: "http://localhost:8888/mama/app"
-    }); 
-    gulp.watch("app/pug/**/*.pug", function () {
-        return gulp.src('app/pug/pages/*.pug')
-            .pipe(pug({
-                pretty: true
-            }))
-            .pipe(gulp.dest('app/'))
-            .pipe(browserSync.reload({stream: true}));
-    });
-    gulp.watch("app/wp-content/themes/mama/**/*.php").on("change", browserSync.reload);
-    gulp.watch("app/wp-content/themes/mama/sass/**/*.scss", function () {
-        return gulp.src("app/wp-content/themes/mama/sass/**/*.scss")
-          .pipe(sass())
-          .pipe(autoprefixer({
-            overrideBrowserslist: ['> 0.1%'],
-            cascade: false
-          }))
-          .pipe(cleanCSS({
-            level: 2
-          }))
-          .pipe(gulp.dest("app/wp-content/themes/mama/css"))
-          .pipe(browserSync.stream());
-    });
-    gulp.watch("app/wp-content/themes/mama/js/**/*.js", function () {
-      return gulp.src("app/wp-content/themes/mama/js/index.js")
-        .pipe(webpack(webConfig))
-        .pipe(gulp.dest("app/wp-content/themes/mama/buildjs"))
-        .pipe(browserSync.stream());
-    });
+    // server: {
+    //     baseDir: "./app/wp-content/themes/mama"
+    // }
+    proxy: "http://localhost:8888/mama/app"
+  });
+  gulp.watch("app/pug/**/*.pug", function () {
+    return gulp.src('app/pug/pages/*.pug')
+      .pipe(pug({
+        pretty: true
+      }))
+      .pipe(gulp.dest('app/'))
+      .pipe(browserSync.reload({stream: true}));
+  });
+  gulp.watch("app/wp-content/themes/mama/**/*.php").on("change", browserSync.reload);
+  gulp.watch("app/wp-content/themes/mama/sass/**/*.scss", function () {
+    return gulp.src("app/wp-content/themes/mama/sass/**/*.scss")
+      .pipe(sass())
+      .pipe(autoprefixer({
+        overrideBrowserslist: ['> 0.1%'],
+        cascade: false
+      }))
+      .pipe(cleanCSS({
+        level: 2
+      }))
+      .pipe(gulp.dest("app/wp-content/themes/mama/css"))
+      .pipe(browserSync.stream());
+  });
+  gulp.watch("app/wp-content/themes/mama/js/**/*.js", function () {
+    return gulp.src("app/wp-content/themes/mama/js/index.js")
+      .pipe(webpack(webConfig))
+      .pipe(gulp.dest("app/wp-content/themes/mama/buildjs"))
+      .pipe(browserSync.stream());
+  });
 });
